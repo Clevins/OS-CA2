f096f5661db95680f5db40dfcf992e846edcfe53
paddyfro <paddyfro@gmail.com>
2020-04-26 17:13:37 +0000

    section 3 task 5 comeplete

--- section_three/gen_report.c
+++ section_three/gen_report.c
@@ -1,18 +1,32 @@
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(){
 	printf("gen report!\n");
 	char str[2000];
 	fgets(str, 2000, stdin);
-	
+	int count = 0;
+	int index = -1;
 	char * pch;
 	pch = strtok (str, " ");
 	while(pch != NULL)
 	{
-		printf("%s\n", pch);
+		if(strcmp(pch, "200")==0){
+
+//			printf("%d,%s\n", count, pch);
+			pch = strtok(NULL, " ");
+			index = count;
+		}
+
+		if(count == index){
+			printf("proc time:%s\n", pch);
+		}
 		pch = strtok(NULL, " ");
+		count++;
 	}
 	return 0;
 }
+
+
+
